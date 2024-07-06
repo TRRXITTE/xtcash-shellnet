f7646c763c89005954ae3a548def55b8692165a7
traaitt | TRRXITTE Int., incorporate <traaitt@icloud.com>
2024-07-06 12:11:49 +0200

    cash address update

--- services/wallet/init.go
+++ services/wallet/init.go
@@ -17,7 +17,7 @@ var (
 
 const (
 	// Forking config.
-	addressFormat          = "^cash([a-zA-Z0-9]{95}|[a-zA-Z0-9]{183})$"
+	addressFormat          = "^cash([a-zA-Z0-9]{94}|[a-zA-Z0-9]{183})$"
 	divisor        float64 = 100 // This is 100 for XTCASH
 	transactionFee         = 100  // This is 10 for XTCASH
 )
